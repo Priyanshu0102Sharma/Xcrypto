deb0928e3d576be0331ca5547359cf1ec086da6d
Priyanshu <priyanshu0102here@gmail.com>
2024-10-26 21:37:40 +0530

    made css changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
-import img from "../assets/img.jpeg"
+import img from "../assets/img.jpg"
 
 // const avatarSrc = "https://avatars.githubusercontent.com/u/25058652";
 
@@ -21,14 +21,15 @@ const Footer = () => {
             letterSpacing={"widest"}
             textAlign={["center", "left"]}
           >
-            We are the best crypto trading app in India, we provide our guidance
-            at a very cheap price.
+            This is the place where you find everything about the crypto world!
           </Text>
         </VStack>
 
         <VStack >
           <Avatar boxSize={"28"} mt={["4", "0"]} src={img} />
+          <a href="https://priyanshusharma01.netlify.app/">
           <Text>Our Founder</Text>
+          </a>
         </VStack>
       </Stack>
     </Box>
